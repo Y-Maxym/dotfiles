28dcd691026b0ccde868c2a25712021bac0701df
maksym <maxym0198@gmail.com>
2024-11-16 17:58:53 +0200

    Update nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.mouse = 'a'
