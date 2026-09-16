4f1939647a640072a0e752c1d5752790a6f5fbf2
Maksym <rain.maks.x@gmail.com>
2026-09-17 01:23:46 +0300

    Add Hyprland config

---
+++ hypr/.config/hypr/modules/programs.lua
@@ -0,0 +1,15 @@
+
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local terminal    = "kitty"
+local fileManager = "yazi"
+local menu        = "hyprlauncher"
+
+return {
+    terminal    = terminal,
+    fileManager = fileManager,
+    menu        = menu,
+}
