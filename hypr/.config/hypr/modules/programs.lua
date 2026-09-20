339db9c745bfc74ce8b5d9caba41fd53ed8c61c6
Maksym <rain.maks.x@gmail.com>
2026-09-20 22:42:07 +0300

    Update config

--- hypr/.config/hypr/modules/programs.lua
+++ hypr/.config/hypr/modules/programs.lua
@@ -6,7 +6,7 @@
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "yazi"
-local menu        = "hyprlauncher"
+local menu        = "rofi -show drun"
 local browser     = "google-chrome-stable"
 
 return {
